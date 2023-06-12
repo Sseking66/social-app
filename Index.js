f22ace736edfcf21e31ff7648118d3a6971a022e
Sseking66 <125562761+Sseking66@users.noreply.github.com>
2023-06-12 20:29:42 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,10 @@
+import react from 'React';
+import reactDOM from 'React-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
